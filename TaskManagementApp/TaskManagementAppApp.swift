4296fab26fe2f561d7f055248c4299736cb49d7d
Dawood Akbar <uzumakiminato806@gmail.com>
2025-01-30 21:32:00 +0500

    Initial Commit

---
+++ TaskManagementApp/TaskManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementAppApp.swift
+//  TaskManagementApp
+//
+//  Created by Dawood Akbar on 30/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
